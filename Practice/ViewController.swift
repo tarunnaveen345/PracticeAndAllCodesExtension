ca75bdb86d2634e32bc51fbc13b912b8abc7bb05
tarun naveen <tarun.kapileswarapu@nannas.co>
2018-07-09 10:23:52 +0530

    1st commit
    1st commit

---
+++ Practice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Practice
+//
+//  Created by tarun naveen on 09/07/18.
+//  Copyright © 2018 tarun naveen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
